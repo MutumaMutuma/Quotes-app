cd45da49d70e7a81a4f60142ed128899907e2f71
MutumaMutuma <lewismutuma1000@gmail.com>
2018-08-10 14:54:34 +0300

    deleted dist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { GoalFormComponent } from './goal-form/goal-form.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 // import { NgbNodule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GoalFormComponent,
     QuoteFormComponent
   ],
   imports: [
