8bd069378afff2dafdc66be1d36cc809200b762e
MutumaMutuma <lewismutuma1000@gmail.com>
2018-08-10 14:26:27 +0300

    added forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { GoalFormComponent } from './goal-form/goal-form.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+// import { NgbNodule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoalFormComponent,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule
+    // NgbNodule
   ],
   providers: [],
   bootstrap: [AppComponent]
