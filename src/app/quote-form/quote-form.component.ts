fec3551474b6c2b6bc60b29921f6d40929513410
MutumaMutuma <lewismutuma1000@gmail.com>
2018-08-10 19:12:18 +0300

    added a quote

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
-
+  quotes = [
+          new Quote(1, 'Things change. And friends leave. Life doesnt stop for anybody.'),
+        ];
   constructor() { }
 
   ngOnInit() {
