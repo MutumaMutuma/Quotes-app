8ad815ef6a15dc77c4d59ec0596e27706d45d61f
MutumaMutuma <lewismutuma1000@gmail.com>
2018-08-10 15:15:23 +0300

    added a new Quote component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteComponent } from './quote/quote.component';
 // import { NgbNodule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteFormComponent
+    QuoteFormComponent,
+    QuoteComponent
   ],
   imports: [
     BrowserModule
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,3 @@
 export class Quote {
+    constructor(public id: number, public name: string) {}
 }
