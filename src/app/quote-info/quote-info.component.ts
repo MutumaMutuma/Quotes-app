b18dc4b2c1b1bf2f8b5a4471b88297edf92f2b68
MutumaMutuma <lewismutuma1000@gmail.com>
2018-08-13 14:22:08 +0300

    added  a dist

--- src/app/quote-info/quote-info.component.ts
+++ src/app/quote-info/quote-info.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { Quote } from '../quote';
+
+
 @Component({
   selector: 'app-quote-info',
   templateUrl: './quote-info.component.html',
   styleUrls: ['./quote-info.component.css']
 })
+
+
 export class QuoteInfoComponent implements OnInit {
   
   upVote() {
